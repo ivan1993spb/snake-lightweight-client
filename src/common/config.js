2ede98d701d546250f1671f701f8a8bdb07340e7
Ivan Pushkin <pushkin13@bk.ru>
2019-01-05 02:53:56 +0300

    Create config

---
+++ src/common/config.js
@@ -0,0 +1,7 @@
+export const API_URL = 'http://localhost:8000/api'
+export default API_URL
+
+export const MOCK_API = process.env.NODE_ENV === 'development'
+
+// TODO: Creat client version and build
+export const SNAKE_CLIENT_NAME = 'SnakeLightweightClient'

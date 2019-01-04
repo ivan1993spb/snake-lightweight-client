7d60031df4da99676e527914e52553e65e2308b7
Ivan Pushkin <pushkin13@bk.ru>
2019-01-05 02:56:10 +0300

    Init application in main

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+
+import ApiService from './common/api.service'
+import DateFilter from './common/date.filter'
+import ErrorFilter from './common/error.filter'
 
 Vue.config.productionTip = false
+Vue.filter('date', DateFilter)
+Vue.filter('error', ErrorFilter)
+
+ApiService.init()
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')

2223ddd6ef02c9bc27c20853dd42ef74358886f8
Ivan Pushkin <pushkin13@bk.ru>
2019-01-05 02:51:54 +0300

    Rewrite routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -9,15 +8,22 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: () => import('@/views/Home.vue')
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import('@/views/About.vue')
+    },
+    {
+      path: '/games',
+      name: 'games',
+      component: () => import('@/views/Games')
+    },
+    {
+      path: '/games/:id',
+      name: 'game',
+      component: () => import('@/views/Game')
     }
   ]
 })

082adc9f81429c5ce62834aa38ad91dc965fdb34
Ivan Pushkin <pushkin13@bk.ru>
2019-01-05 02:52:49 +0300

    Create component GameItem

---
+++ src/components/GameItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <span>
+      <router-link :to="{ name: 'game', params: { id } }">Game {{ id }}</router-link>
+    </span>
+    <span>
+      <b>Map: {{ width }}x{{ height }}</b>
+    </span>
+    <span>
+      <b>Players: {{ count }}/{{ limit }}</b>
+    </span>
+    <span>rate: {{ rate }}</span>
+    <span v-if="count===0">
+      <i>delete</i>
+    </span>
+    <span v-else>
+      <s>delete</s>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameItem',
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    width: {
+      type: Number,
+      required: true
+    },
+    height: {
+      type: Number,
+      required: true
+    },
+    count: {
+      type: Number,
+      required: true
+    },
+    limit: {
+      type: Number,
+      required: true
+    },
+    rate: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+span {
+  margin: 20px;
+}
+</style>

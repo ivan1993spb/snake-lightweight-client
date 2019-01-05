f60f43d264200e77a98497fbdb54a7630ca179a1
Ivan Pushkin <pushkin13@bk.ru>
2019-01-05 05:25:07 +0300

    Fix capacity representation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1>Home</h1>
     <div>Welcome to the snake arcade game!</div>
-    <div>Server capacity: {{ capacity.toFixed(2) }}%</div>
+    <div>Server capacity: {{ capacity > 1 ? capacity.toFixed(0) : capacity.toFixed(2) }}%</div>
   </div>
 </template>
 

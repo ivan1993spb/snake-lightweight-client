0650dff25bdcd23b12cd8d5a9181019ad9644f16
Ivan Pushkin <pushkin13@bk.ru>
2019-01-07 00:25:26 +0300

    Create details into Play view; Create Playground component

---
+++ src/components/Playground.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <canvas ref="canvas-snakes" width="200" height="200"></canvas>
+    <canvas ref="canvas-food" width="200" height="200"></canvas>
+    <canvas ref="canvas-walls" width="200" height="200"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Playground',
+
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    width: {
+      type: Number,
+      required: true
+    },
+    height: {
+      type: Number,
+      required: true
+    }
+  },
+
+  mounted () {
+    // TODO: Use different canvases for different objects.
+    const contextSnakes = this.$refs['canvas-snakes'].getContext('2d', { alpha: false })
+    // const contextFood = this.$refs['canvas-food'].getContext('2d', { alpha: false })
+    // const contextWalls = this.$refs['canvas-walls'].getContext('2d', { alpha: true })
+    console.log(contextSnakes)
+    console.log(this.id, this.width, this.height)
+  }
+}
+</script>
--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,5 +1,51 @@
 <template>
   <div>
-    <h1>Play game</h1>
+    <h1>Play game {{ game.id }}</h1>
+    <div>
+      <span><b>Details</b></span>
+      <span>Players: {{ game.count }}/{{ game.limit }}</span>
+      <span>Messages: {{ game.rate }} per sec</span>
+    </div>
+    <div>
+      <Playground :width="game.width" :height="game.height" :id="game.id"/>
+    </div>
   </div>
 </template>
+
+<script>
+
+import { mapGetters } from 'vuex'
+import Playground from '@/components/Playground'
+import { UPDATE_GAME } from '@/store/actions.type'
+import store from '@/store'
+
+export default {
+  name: 'play',
+  computed: {
+    ...mapGetters(['game'])
+  },
+  components: {
+    Playground
+  },
+  methods: {
+    updateGame () {
+      store.dispatch(UPDATE_GAME, this.game.id)
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+    Promise.all([
+      store.dispatch(UPDATE_GAME, to.params.id)
+    ]).then(() => {
+      next()
+    })
+  },
+  mounted () {
+    this.gameUpdateInterval = setInterval(() => {
+      this.updateGame()
+    }, 10000)
+  },
+  beforeDestroy () {
+    clearInterval(this.gameUpdateInterval)
+  }
+}
+</script>

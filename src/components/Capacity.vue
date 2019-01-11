e2ec3e601c9d605342b341460138f27a6fd6d747
Ivan Pushkin <pushkin13@bk.ru>
2019-01-12 01:37:18 +0300

    Create Capacity component

---
+++ src/components/Capacity.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="capacity">
+    <div>Server capacity</div>
+    <div>{{ capacity > 1 ? capacity.toFixed(0) : capacity.toFixed(2) }}</div>
+    <div>%</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'capacity',
+  props: {
+    capacity: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.capacity {
+  margin: 10px;
+
+  div {
+    display: inline-block;
+    margin: 2px;
+  }
+}
+</style>

6eda90319ac4e2d2a8705b4620ad222d80cee81f
Ivan Pushkin <pushkin13@bk.ru>
2019-01-12 01:36:36 +0300

    Create unit test for GameItem component

---
+++ tests/unit/GameItem.spec.js
@@ -0,0 +1,18 @@
+import { shallowMount } from '@vue/test-utils'
+import GameItem from '@/components/GameItem.vue'
+
+describe('GameItem', () => {
+  it('renders props.msg when passed', () => {
+    const wrapper = shallowMount(GameItem, {
+      propsData: {
+        id: 1,
+        width: 10,
+        height: 10,
+        count: 10,
+        limit: 10,
+        rate: 10
+      }
+    })
+    expect(wrapper.text()).toBe("Game 1 Map: 10x10 Players: 10/10 rate: 10 delete")
+  })
+})

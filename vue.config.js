428265fa594ac5d8201469b3821036e76c1123b4
Ivan Pushkin <pushkin13@bk.ru>
2019-01-13 18:23:46 +0300

    Create env vars for author and licanse in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack')
 const GitRevisionPlugin = require('git-revision-webpack-plugin')
+const packageJson = require('./package.json')
 
 const gitRevisionPlugin = new GitRevisionPlugin({
   versionCommand: 'describe --tags --abbrev=0',
@@ -8,7 +9,9 @@ const gitRevisionPlugin = new GitRevisionPlugin({
 
 const environmentPlugin = new webpack.EnvironmentPlugin({
   VERSION: gitRevisionPlugin.version(),
-  BUILD: gitRevisionPlugin.commithash()
+  BUILD: gitRevisionPlugin.commithash(),
+  LICENSE: packageJson['license'],
+  AUTHOR: packageJson['author']['name']
 });
 
 module.exports = {

48ea649dd18f2449370754a36f4f0b2d88c19054
Ivan Pushkin <pushkin13@bk.ru>
2019-02-02 23:47:30 +0300

    Rename WebSocketMock class

---
+++ src/mocks/WebSocketMock.js
@@ -0,0 +1,65 @@
+
+const MESSAGE_CLOSE = {
+  code: 1000,
+  reason: '',
+  type: 'close'
+}
+
+const MESSAGE_OPEN = {
+  type: 'open'
+}
+
+const MESSAGE_RECEIVE_DELAY = 500
+
+export class WebSocketMock {
+  constructor (socketUrl, gameReplay) {
+    this._game = gameReplay
+
+    this.onmessage = () => {
+      throw new Error('method to be triggered is not specified: onmessage')
+    }
+    this.onclose = () => {
+      throw new Error('method to be triggered is not specified: onclose')
+    }
+    this.onerror = () => {
+      throw new Error('method to be triggered is not specified: onerror')
+    }
+    this.onopen = () => {
+      throw new Error('method to be triggered is not specified: onopen')
+    }
+
+    this._intervalId = 0
+
+    this._start()
+  }
+
+  _start () {
+    this._timeoutId = setTimeout(() => {
+      this.onopen(MESSAGE_OPEN)
+
+      this._intervalId = setInterval(() => {
+        this.onmessage({
+          type: 'message',
+          data: JSON.stringify(this._game.next())
+        })
+      }, MESSAGE_RECEIVE_DELAY)
+    }, MESSAGE_RECEIVE_DELAY)
+  }
+
+  _stop () {
+    clearTimeout(this._timeoutId)
+
+    clearInterval(this._intervalId)
+
+    this.onclose(MESSAGE_CLOSE)
+  }
+
+  close () {
+    this._stop()
+  }
+
+  send (data) {
+  }
+}
+
+export default WebSocketMock

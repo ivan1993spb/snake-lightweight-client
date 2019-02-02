779b91bea7f3b14323dffd6de73749eb0c261ec8
Ivan Pushkin <pushkin13@bk.ru>
2019-02-02 23:44:31 +0300

    Create CanvasFactory class

---
+++ src/game/CanvasFactory.js
@@ -0,0 +1,34 @@
+
+import Canvas from './Canvas'
+
+export class CanvasFactory {
+  constructor ({ canvasSnakes, canvasFood, canvasWalls, canvasGrid }) {
+    this._canvasSnakes = canvasSnakes
+    this._canvasFood = canvasFood
+    this._canvasWalls = canvasWalls
+    this._canvasGrid = canvasGrid
+
+    this._initContexts()
+  }
+
+  _initContexts () {
+    this._contextSnakes = this._canvasSnakes.getContext('2d', { alpha: false })
+    this._contextFood = this._canvasFood.getContext('2d', { alpha: true })
+    this._contextWalls = this._canvasWalls.getContext('2d', { alpha: true })
+    this._contextGrid = this._canvasGrid.getContext('2d', { alpha: true })
+  }
+
+  createCanvas (grid) {
+    return new Canvas({
+      contexts: {
+        contextSnakes: this._contextSnakes,
+        contextFood: this._contextFood,
+        contextWalls: this._contextWalls,
+        contextGrid: this._contextGrid
+      },
+      grid
+    })
+  }
+}
+
+export default CanvasFactory

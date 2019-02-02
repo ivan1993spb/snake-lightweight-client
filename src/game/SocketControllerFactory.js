6224894e1f630cb4abfd881f9c1cc3d22e418f4e
Ivan Pushkin <pushkin13@bk.ru>
2019-02-03 00:16:31 +0300

    Fix SocketControllerFactory to use WebSocketFactory

--- src/game/SocketControllerFactory.js
+++ src/game/SocketControllerFactory.js
@@ -4,8 +4,10 @@ import urljoin from 'url-join'
 import {
   WS_URL, MOCK_WS, SERVER_MESSAGES_COUNTER_PERIOD_SEC
 } from '@/common/config'
-import SocketController from './SocketController'
 import WebSocketMock from '@/mocks/WebSocketMock'
+import LoopGameReplay from '@/mocks/LoopGameReplay'
+import WebSocketFactory from './WebSocketFactory'
+import SocketController from './SocketController'
 
 const ENABLE_MESSAGE_COUNT_LOGGING = log.getLevel() <= log.levels.DEBUG
 
@@ -14,17 +16,20 @@ export class SocketControllerFactory {
     this._id = id
     this._socketURL = urljoin(WS_URL, 'games', this._id.toString())
 
+    this._initWebSocketFactory()
+  }
+
+  _initWebSocketFactory () {
     if (MOCK_WS) {
-      this._wsClass = WebSocketMock
+      this._webSocketFactory = new WebSocketFactory(WebSocketMock, this._socketURL, new LoopGameReplay())
     } else {
-      this._wsClass = WebSocket
+      this._webSocketFactory = new WebSocketFactory(WebSocket, this._socketURL)
     }
   }
 
   create () {
     return new SocketController({
-      wsUrl: this._socketURL,
-      wsClass: this._wsClass,
+      webSocketFactory: this._webSocketFactory,
       srvMsgCounterEnable: ENABLE_MESSAGE_COUNT_LOGGING,
       srvMsgCounterPeriodSec: SERVER_MESSAGES_COUNTER_PERIOD_SEC
     })

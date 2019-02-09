8ec9805d9dec83e168e4e262801e79b376f259a1
Ivan Pushkin <pushkin13@bk.ru>
2019-02-09 22:22:51 +0300

    Add methods start and stop to ReplayWebSocketMock

--- src/mocks/ReplayWebSocketMock.js
+++ src/mocks/ReplayWebSocketMock.js
@@ -1,19 +1,50 @@
 
 import BaseWebSocketMock from './BaseWebSocketMock'
 
+const START_GAME_STREAM_DELAY = 50
+
+const MESSAGE_CLOSE = {
+  code: 1000,
+  reason: '',
+  type: 'close'
+}
+
+const MESSAGE_OPEN = {
+  type: 'open'
+}
+
 export class ReplayWebSocketMock extends BaseWebSocketMock {
   constructor (replay) {
     super()
 
     this._replay = replay
-    this._replay.onServerGameMessage = message => {
-      this.onmessage(JSON.stringify(message))
+    this._timeoutId = 0
+    const onServerGameMessage = message => {
+      this.onmessage({
+        type: 'message',
+        data: JSON.stringify(message)
+      })
     }
-    this._replay.start()
+    this._replay.onServerGameMessage = onServerGameMessage
+
+    this._start()
+  }
+
+  _start () {
+    this._timeoutId = setTimeout(() => {
+      this.onopen(MESSAGE_OPEN)
+      this._replay.start()
+    }, START_GAME_STREAM_DELAY)
+  }
+
+  _stop () {
+    clearTimeout(this._timeoutId)
+    this._replay.stop()
+    this.onclose(MESSAGE_CLOSE)
   }
 
   close () {
-    this._replay.stop()
+    this._stop()
   }
 }
 

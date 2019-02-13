eb49a545be4d8b6760ef018a28aa176ad4245c5b
Ivan Pushkin <pushkin13@bk.ru>
2019-02-13 04:23:25 +0300

    Fixing configs to get default endpoints correctly

--- src/common/config.js
+++ src/common/config.js
@@ -1,6 +1,11 @@
 
 const envDev = process.env.NODE_ENV === 'development'
 
+const DEFAULT_SNAKE_SERVER_HOST = location.hostname || 'localhost'
+const DEFAULT_SNAKE_SERVER_PORT = location.port || '8080'
+const DEFAULT_SNAKE_SERVER_WEB_SCHEME = location.protocol || 'http'
+const DEFAULT_SNAKE_SERVER_SOCKET_SCHEME = location.protocol === 'https' ? 'wss' : 'ws'
+
 export const VERSION = process.env.VERSION
 export const BUILD = process.env.BUILD
 export const LICENSE = process.env.LICENSE
@@ -8,10 +13,10 @@ export const AUTHOR = process.env.AUTHOR
 
 export const LOG_LEVEL = envDev ? 'trace' : 'silent'
 
-export const SNAKE_SERVER_HOST = process.env.SNAKE_SERVER_HOST || 'localhost'
-export const SNAKE_SERVER_PORT = process.env.SNAKE_SERVER_PORT || '8080'
-export const SNAKE_SERVER_WEB_SCHEME = process.env.SNAKE_SERVER_WEB_SCHEME || 'http'
-export const SNAKE_SERVER_SOCKET_SCHEME = process.env.SNAKE_SERVER_SOCKET_SCHEME || 'ws'
+export const SNAKE_SERVER_HOST = process.env.SNAKE_SERVER_HOST || DEFAULT_SNAKE_SERVER_HOST
+export const SNAKE_SERVER_PORT = process.env.SNAKE_SERVER_PORT || DEFAULT_SNAKE_SERVER_PORT
+export const SNAKE_SERVER_WEB_SCHEME = process.env.SNAKE_SERVER_WEB_SCHEME || DEFAULT_SNAKE_SERVER_WEB_SCHEME
+export const SNAKE_SERVER_SOCKET_SCHEME = process.env.SNAKE_SERVER_SOCKET_SCHEME || DEFAULT_SNAKE_SERVER_SOCKET_SCHEME
 
 const skipPort = (SNAKE_SERVER_PORT === '80' && SNAKE_SERVER_WEB_SCHEME === 'http') ||
   (SNAKE_SERVER_PORT === '443' && SNAKE_SERVER_WEB_SCHEME === 'https')

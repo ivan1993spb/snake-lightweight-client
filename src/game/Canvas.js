a784109d29c3c8e51888c76a9907033c46bc233e
Ivan Pushkin <iv.pushk@gmail.com>
2025-10-02 21:35:00 -0400

    refactor(game): simplify canvas draw color

--- src/game/Canvas.js
+++ src/game/Canvas.js
@@ -120,38 +120,33 @@ export class Canvas {
     return this._border + this._dot * dotY + this._line * (dotY + 1)
   }
 
-  draw (type, dots) {
+  _getObjectColor (type) {
     switch (type) {
       case OBJECT_PLAYER:
-        this._draw(this._contextGame, COLOR_PLAYER, dots)
-        break
+        return COLOR_PLAYER
       case OBJECT_SNAKE:
-        this._draw(this._contextGame, COLOR_SNAKE, dots)
-        break
+        return COLOR_SNAKE
       case OBJECT_HIGHLIGHTED:
-        this._draw(this._contextGame, COLOR_HIGHLIGHTED, dots)
-        break
+        return COLOR_HIGHLIGHTED
       case OBJECT_APPLE:
-        this._draw(this._contextGame, COLOR_APPLE, dots)
-        break
+        return COLOR_APPLE
       case OBJECT_CORPSE:
-        this._draw(this._contextGame, COLOR_CORPSE, dots)
-        break
+        return COLOR_CORPSE
       case OBJECT_WATERMELON:
-        this._draw(this._contextGame, COLOR_WATERMELON, dots)
-        break
+        return COLOR_WATERMELON
       case OBJECT_WALL:
-        this._draw(this._contextGame, COLOR_WALL, dots)
-        break
+        return COLOR_WALL
       case OBJECT_MOUSE:
-        this._draw(this._contextGame, COLOR_MOUSE, dots)
-        break
+        return COLOR_MOUSE
       case OBJECT_UNKNOWN:
-        this._draw(this._contextGame, COLOR_UNKNOWN, dots)
-        break
+        return COLOR_UNKNOWN
       default:
-        throw new Error(`Canvas.draw: invalid type ${type}`)
+        throw new Error(`Canvas._getObjectColor: invalid type ${type}`)
     }
+  }
+
+  draw (type, dots) {
+    this._draw(this._contextGame, this._getObjectColor(type), dots)
   }
 
   _draw (context, color, dots) {
